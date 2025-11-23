98be9beaa207f9595652e2d4646d3a5a42679509
Alfonso.abbott <alfonso.abbott@hotmail.com>
2025-11-23 20:32:35 -0300

    Primer commit proyecto de título

---
+++ lanzar_dashboard.bat
@@ -0,0 +1,13 @@
+@echo off
+REM Ir a la carpeta donde está este .bat
+cd /d "%~dp0"
+
+echo Activando entorno virtual...
+
+REM Ruta explícita al python del entorno virtual
+set PYTHON_VENV=%~dp0.venv\Scripts\python.exe
+
+echo Ejecutando el dashboard interactivo...
+"%PYTHON_VENV%" "scripts_final\app_dashboard.py"
+
+pause
